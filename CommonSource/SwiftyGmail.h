f613c0ddd72f984fcd69596264a4d71179b4298c
ataibarkai <atai.barkai@gmail.com>
2016-02-09 19:02:25 +0200

    Initial commit
    Before attempting renaming of project to discover missing flag to change

---
+++ CommonSource/SwiftyGmail.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyGmail_iOS.h
+//  SwiftyGmail_iOS
+//
+//  Created by Atai Barkai on 2/8/16.
+//  Copyright © 2016 Atai Barkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyGmail_iOS.
+FOUNDATION_EXPORT double SwiftyGmail_iOSVersionNumber;
+
+//! Project version string for SwiftyGmail_iOS.
+FOUNDATION_EXPORT const unsigned char SwiftyGmail_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyGmail_iOS/PublicHeader.h>
+
+
